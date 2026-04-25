2c958900202b311de7ac0c81d5ec6a820e0ecdb3
GermanG <gamarragerman19@gmail.com>
2026-04-25 14:54:50 -0300

    Entrega 1 final - wallet + tokens + sepolia

--- Entregable1Taller/src/App.tsx
+++ Entregable1Taller/src/App.tsx
@@ -1,11 +1,26 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-
+import { useAccount } from 'wagmi'
+import { AccountPanel } from './components/AccountPanel'
+import { TokenBalances } from './components/TokenBalances'
 function App() {
+  const { isConnected } = useAccount()
+
+  if (!isConnected) {
+    return (
+      <main style={{ padding: 40 }}>
+        <ConnectButton />
+      </main>
+    )
+  }
+
   return (
-    <div style={{ padding: 40 }}>
+    <main style={{ padding: 40 }}>
       <h1>Web3 App</h1>
       <ConnectButton />
-    </div>
+
+      <AccountPanel />
+      <TokenBalances />
+    </main>
   )
 }
 
